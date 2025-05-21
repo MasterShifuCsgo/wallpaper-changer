4da87876aac1b7a65c6cd7ce186433f3820a4931
Kaspar Bergert <kasparbergert6@gmail.com>
2025-05-21 23:14:53 +0300

    has functionality to fetch an image from the internet

--- Readme.md
+++ Readme.md
@@ -3,5 +3,5 @@ What you must have
 
 a folder which holds all the wallpapers in JPG format.
 Must be Jpg format! 
-design is like this becuase windows stores the TranscodedWallpaper in jpg format without extension
+design is like this becuase windows stores the TranscodedWallpaper in jpg. without showing the extension of course.
 
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,24 @@
+use std::fs;
+use std::{io::Write, path::PathBuf, thread, time};
+
 use fastrand;
-use std::path::PathBuf;
+use reqwest;
+use tokio;
 
 use std::ffi::OsStr;
 use std::os::windows::ffi::OsStrExt;
-use windows::{    
-    Win32::UI::WindowsAndMessaging::{
-        SystemParametersInfoW, SPI_SETDESKWALLPAPER, SPIF_SENDWININICHANGE, SPIF_UPDATEINIFILE,
-    },
+use windows::Win32::UI::WindowsAndMessaging::{
+    SPI_SETDESKWALLPAPER, SPIF_SENDWININICHANGE, SPIF_UPDATEINIFILE, SystemParametersInfoW,
 };
+
+// CFG
+const SAVE_FETCHED_FILE: bool = false; // (true) when an image is fetched from the internet, it creats a copy of it instead rather than overwrite. 
 
 
 // 1. puts all the jpg files in to a vector.
 // 2. then chooses a random number
 // 3. returns the random jpg path.
-fn get_random_wallpaper(desktop_images: &PathBuf) -> Result<PathBuf, &str> {    
-
+fn get_random_wallpaper(desktop_images: &PathBuf) -> Result<PathBuf, &str> {
     //includes all the directories of the jpg files.
     let mut wallpapers: Vec<PathBuf> = Vec::new();
 
@@ -38,12 +42,53 @@ fn get_random_wallpaper(desktop_images: &PathBuf) -> Result<PathBuf, &str> {
     }
 
     // choosing random number
-    let num = fastrand::usize(..wallpapers.len());    
+    let num = fastrand::usize(..wallpapers.len());
 
     return Ok(wallpapers[num].clone());
 }
 
+// 1. gets data from api
+// 2. creates directory and creates a file to put the picture in.
+// 3. send the Result.
+async fn fetch_wallpaper(i: u32) -> Result<PathBuf, String> {    
+    let mut file_path:String = "./tempImage/TranscodedWallpaper.jpg".to_string();
 
+    if SAVE_FETCHED_FILE {
+        file_path = format!("./tempImage/{}.jpg", i).to_string();
+    }
+
+    // get image from api
+    let data = reqwest::get("https://picsum.photos/1920/1080")
+        .await
+        .map_err(|err| format!("No respone from API: {}", err))?
+        .bytes()
+        .await
+        .map_err(|err| format!("Failed to make response into bytes: {}", err))?;
+
+    fs::create_dir_all("./tempImage").map_err(|err| format!("Failed creating ./tempImage dir: {}", err))?;        
+
+    let mut file = fs::File::create(&file_path)
+        .map_err(|e| format!("Failed to create file in ./tempImage directory: {}", e))?; // creates or truncates the file.            
+
+    file.write_all(&data).map_err(|err| {
+        format!(
+            "Failed to write data to created file 'fetch_wallpaper': {}",
+            err
+        )
+    })?;
+
+    let absolute_path = PathBuf::from(file_path).canonicalize().map_err(|err| {
+        format!(
+            "Error occured when retrieveing files absolute path: {}",
+            err
+        )
+    })?;
+
+    Ok(absolute_path)
+}
+
+
+// uses windows SystemParametersInfoW to set wallpaper
 fn set_wallpaper(path: &PathBuf) {
     let wide: Vec<u16> = OsStr::new(path)
         .encode_wide()
@@ -62,22 +107,33 @@ fn set_wallpaper(path: &PathBuf) {
         .unwrap();
     }
 }
-fn main() {
+
+fn sleep(m: u64){
+    std::thread::sleep(std::time::Duration::from_millis(m));
+}
+
+#[tokio::main]
+async fn main() {
+
     let wallpapers_dir =
         PathBuf::from("C:/Users/kaspar/Desktop/Kaspar Files/Personal folder/Desktop images");
 
-    loop {
+    for i in 0.. {
+        sleep(500);
+        let wallpaper_path1 = get_random_wallpaper(&wallpapers_dir);
+        let wallpaper_path2 = fetch_wallpaper(i).await;
 
-    let wallpaper_path = get_random_wallpaper(&wallpapers_dir);
+        match wallpaper_path2 {
+            Ok(path) => {
+                //apply the random wallpaper to windows wallpaper
 
-    match wallpaper_path {
-        Ok(path) => {
-            //apply the random wallpaper to windows wallpaper
-            set_wallpaper(&path);
+                println!("Setting wallpaper: {}", path.display());
+                set_wallpaper(&path);
+                print!("Wallpaper set\n");
+            }
+            Err(err) => {
+                println!("ERR: {}", err);
+            }
         }
-        Err(err) => {
-            println!("ERR: {}", err);
-        }
-    }
     }
 }
